38163b5b259a92ef27c80ca90b84153506035907
kassab3m <kassab3m@gmail.com>
2021-01-07 18:12:50 +0800

    add firebase

---
+++ js/fb.js
@@ -0,0 +1,15 @@
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBK9Hz2SHrfLrIzRxi3ZHwL_smgre1VeDE",
+  authDomain: "pbhci-a0fa0.firebaseapp.com",
+  projectId: "pbhci-a0fa0",
+  storageBucket: "pbhci-a0fa0.appspot.com",
+  messagingSenderId: "44778468550",
+  appId: "1:44778468550:web:17ff3d593fce69125913c7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
